8489e98d4f80e7a1578eb8e63b80f75a10516e4e
graceeekim <gracekimj02@msn.com>
2020-11-02 10:35:05 -0500

    Update jQuery.js

--- js/jQuery.js
+++ js/jQuery.js
@@ -1,7 +1,11 @@
+// issues I need to fix:
+
+// next step: make div for about blurbs, create grid for that, find way to align to bottom of photo;
+// resizing padding issues
 $(window).on("load", function() {
 
 //  VARIABLES
-    var mobileBreakpoint = 481;
+    var mobileBreakpoint = 600;
     var windowWidth = $(window).width(); 
     var scrollTop = window.scrollY;
     var windowIsMobile = windowWidth < mobileBreakpoint;
@@ -11,64 +15,119 @@ $(window).on("load", function() {
     var hidingMenu;
     var hidingBlurb;
 
-//  FUNCTIONS
+ // FUNCTIONS
     function hideShowMenuScroll() {
         var currentScrollPos = scrollTop;     
         if (scrolling === true && prevScrollPos < currentScrollPos && currentScrollPos > 2) {
-            $('#header-nav').hide("fade", { direction: "down"}, 100);
+            $('#header').hide("fade", { direction: "down"}, 200);
         } else if (scrolling === false && prevScrollPos < currentScrollPos && currentScrollPos > 2) {
-            $('#header-nav').hide("fade", { direction: "down"}, 100); 
+            $('#header').hide("fade", { direction: "down"}, 200); 
         } else if (scrolling === true && prevScrollPos > currentScrollPos || scrollTop <= 0) {
-            $('#header-nav').show("fade", { direction: "up"}, 100);
+            $('#header').show("fade", { direction: "up"}, 200);
         } else if (scrolling === false && prevScrollPos > currentScrollPos || scrollTop <= 0) {
-            $('#header-nav').show("fade", { direction: "up"}, 100); 
+            $('#header').show("fade", { direction: "up"}, 200); 
         }
         prevScrollPos = currentScrollPos;  
     } 
     function mobileMenuUX() {
-        var contentPlacement = $('#header').position().top + $('#header').height();
+        blurbPlacement = $('#header').height()
+        contentPlacement = $('#header').height() + $("#sidebar").height() + 70;
 
-        if (window.location.href.indexOf('about')) {
-            $('#sidebar').css('padding-top', contentPlacement); 
-        }        
-        $('#main-content').css('padding-top', contentPlacement); 
-        $('#main-content').css('padding-top', "0"); 
+        if (window.location.href.indexOf('about') > -1) {
+            $('#sidebar').css('padding-top', blurbPlacement); 
+        }   
+        if (window.location.href.indexOf('index') > -1) {
+            $('#sidebar').css('padding-top', blurbPlacement);
+            $('#main-content').css('padding-top', contentPlacement); 
+        }
+        if (window.location.href.indexOf('project') > -1) {
+            $('#sidebar').css('padding-top', blurbPlacement - 30);
+        }       
         $(window).scroll(function() {
             scrolling = true; 
         })    
-        hidingMenu = setInterval(hideShowMenuScroll, 100);
+        hidingMenu = setInterval(hideShowMenuScroll, 200);
+        hidingBlurb = setInterval(hideBlurbOnScroll, 200);
     }
     function desktopMenuUX() {   
-        clearInterval(hidingMenu);      
-        var contentPlacement = $('#header-nav').outerHeight(true) + 20;
-         if (window.location.href.indexOf('about')) {
-            $('#sidebar').css('padding-top', 0); 
-        }   
-        $('#main-content').css('padding-top', contentPlacement);         
-        $('#header-nav').show("fade", { direction: "up"}, 100);  
-        $('#header-blurb').show("fade", { direction: "down"}, 100);  
-            // incase window is resized when these are hidden
-            // need to incorporate tablet sizing, now permanently visible when in tablet sizing 
+        clearInterval(hidingMenu);
+        clearInterval(hidingBlurb);         
+       
+        $('#header').show();
     }
     function checkScreenSize() {
-        windowWidth = $(window).width();  
+        var windowWidth = $(window).width();  
         if (windowWidth < mobileBreakpoint) {
             mobileMenuUX();          
         } else {;
             desktopMenuUX();
-        }              
+        }     
+        contentPlacement = $('#header').outerHeight(true);         
+    }
+    function projectCarouselPos() {
+        contentPlacement = $('#header').outerHeight(true) + 20;
+        spacerHeight = $(window).height() - contentPlacement - $("#project-carousel").outerHeight(true);
+
+        if (window.location.href.indexOf('project') > -1 && windowIsMobile) {
+            $('#pre-carousel-spacer').css('height', contentPlacement);             
+            $('#post-carousel-spacer').css('height', spacerHeight - 33);             
+        } else {
+            $('#pre-carousel-spacer').css('height', contentPlacement); 
+            $('#post-carousel-spacer').css('height', spacerHeight); 
+        }       
     }
 
-// CODE TO RUN WHEN EVERYTHING IS LOADED 
-    $(window).scroll(function() {
-        scrollTop = window.scrollY;
-    })
+// CODE TO RUN WHEN EVERYTHING IS LOADED
 
-    checkScreenSize();
+    // $('.project-container').hover(function() {
+    //     $(this).find("img.thumbnail").css("transform", "scale(1.05");
+    // }, function() {
+    //     $(this).find("img.thumbnail").css("transform", "scale(1");
+    // })
+   if (window.location.href.indexOf('about') > -1 ) {
+        $("#menu-about").css("text-decoration", "underline");
+   }
+   if (window.location.href.indexOf('index') > -1 ) {
+        $("#menu-work").css("text-decoration", "underline");
+   }   
+    // $(window).scroll(function() {
+    //     scrollTop = window.scrollY;
+    // }) 
 
-    $(window).resize(function() {
-        clearInterval(hidingMenu);
-        clearInterval(hidingBlurb);
-        checkScreenSize();   
+    // checkScreenSize();
+    // projectCarouselPos();    
+
+    // $("#menu-about").click(function(e) {
+    //     e.preventDefault();
+
+    //     $("#sidebar").load("about.html #sidebar", function() {
+    //         alert("load was performed");
+    //     });
+    // });
+
+    // $(window).resize(function() {
+    //     clearInterval(hidingMenu);
+    //     clearInterval(hidingBlurb);
+    //     checkScreenSize();  
+    //     projectCarouselPos();
+
+
+    // // $("body").on("click", "#menu-about", function() {
+    // //     var href = $(this).attr("href");
+    // //     $("#main-content").load(href);
+    // //     return false;
+    // // }) 
+    // //     var paddingSpace = $(window).height() - ($("#header").height() + $("#sidebar").height() + $("#footer").height() + 60);
+    // // $("#contact").css("padding-top", paddingSpace);  
+    // })
+    // var paddingSpace = $(window).height() - ($("#header").height() + $("#sidebar").height() + $("#footer").height() + 60);
+    // $("#contact").css("padding-top", paddingSpace);    
+});
+$(document).ready(function() { 
+    $('.project-container').on("mouseenter mousedown touchstart touchmove", function(){
+        $(this).find("img.thumbnail").css("transform", "scale(1.05)");    
     })
+    $('.project-container').on("mouseleave touchstart touchmove", function(){
+        $(this).find("img.thumbnail").css("transform", "scale(1)");    
+    })      
 });
