20d705032c4c03e11e84164b2c0b77831a6fa25a
graceeekim <gracekimj02@msn.com>
2021-03-26 15:51:20 -0400

    Add files via upload

--- js/jQuery.js
+++ js/jQuery.js
@@ -22,26 +22,26 @@ $(document).ready(function() {
         }        
         prevScrollPos = currentScrollPos;  
     }   
-    function checkMedia(){
-        if (url.indexOf("raw-pet-food") > -1) {
-            var scrollTop = $(window).scrollTop() + tolerancePixel;
-            var scrollBottom = $(window).scrollTop() + $(window).height() - tolerancePixel;
+    // function checkMedia(){
+    //     if (url.indexOf("raw-pet-food") > -1) {
+    //         var scrollTop = $(window).scrollTop() + tolerancePixel;
+    //         var scrollBottom = $(window).scrollTop() + $(window).height() - tolerancePixel;
 
-            media.each(function(index, el) {
-                var yTopMedia = $(this).offset().top;
-                var yBottomMedia = $(this).height() + yTopMedia;
+    //         media.each(function(index, el) {
+    //             var yTopMedia = $(this).offset().top;
+    //             var yBottomMedia = $(this).height() + yTopMedia;
 
-                if(scrollTop < yBottomMedia && scrollBottom > yTopMedia){
-                    $(this).get(0).play();
-                } else {
-                    $(this).get(0).pause();
-                }
-            }, setInterval(1000));
-        } else {}
-    }
-    function checkPausePlay() {
-        $(document).on('scroll', checkMedia);
-    }        
+    //             if(scrollTop < yBottomMedia && scrollBottom > yTopMedia){
+    //                 $(this).get(0).play();
+    //             } else {
+    //                 $(this).get(0).pause();
+    //             }
+    //         }, setInterval(1000));
+    //     } else {}
+    // }
+    // function checkPausePlay() {
+    //     $(document).on('scroll', checkMedia);
+    // }        
     function hideShowMenuScroll() {
         $(window).scroll(function() {
             scrolling = true;          
@@ -172,7 +172,7 @@ $(document).ready(function() {
     //   
     //    
     //
-    checkPausePlay();   
+    // checkPausePlay();   
     pageNavCSS();
     staticRevealHideClass();
     scalePicOnHover();
