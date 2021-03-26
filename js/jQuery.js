8711f77f3bfc742422f8f01f87bad4241a54e211
graceeekim <gracekimj02@msn.com>
2021-03-26 11:53:41 -0400

    Add files via upload

--- js/jQuery.js
+++ js/jQuery.js
@@ -3,20 +3,10 @@ $(document).ready(function() {
     var scrolling = false;    
     var prevScrollPos = 0;
     var hidingMenu;
-    var url = window.location.pathname;
-    //
-    function changeHanbanCut(){
-        $("#hanCut").click(function(){
-            $("#hanbanPreview").css("font-family", "hanbanhan");
-            $("#hanCut").addClass("activeHanbanCut")
-            $("#banCut").removeClass("activeHanbanCut")
-        })
-        $("#banCut").click(function(){
-            $("#hanbanPreview").css("font-family", "hanbanban");
-            $("#banCut").addClass("activeHanbanCut")
-            $("#hanCut").removeClass("activeHanbanCut")
-        })    
-    }        
+    var url = window.location.pathname; 
+    var media = $("video").not("[autoplay='autoplay']");
+    var tolerancePixel = 40;    
+    //      
     function checkScrollPos() {
         var currentScrollPos = scrollTop;     
         if (scrolling === true && prevScrollPos < currentScrollPos && currentScrollPos > 2) {
@@ -27,11 +17,31 @@ $(document).ready(function() {
             $('#header').show("fade", {direction: "up"}, 200);
         } else if (scrolling === false && prevScrollPos > currentScrollPos || scrollTop <= 0) {
             $('#header').show("fade", {direction: "up"}, 200); 
-        }   else if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+        }  else if ($(window).scrollTop() + $(window).height() == $(document).height() || window.scrollY == 0){
             $('#header').show("fade", {direction: "up"}, 200);
         }        
         prevScrollPos = currentScrollPos;  
-    }     
+    }   
+    function checkMedia(){
+        if (url.indexOf("raw-pet-food") > -1) {
+            var scrollTop = $(window).scrollTop() + tolerancePixel;
+            var scrollBottom = $(window).scrollTop() + $(window).height() - tolerancePixel;
+
+            media.each(function(index, el) {
+                var yTopMedia = $(this).offset().top;
+                var yBottomMedia = $(this).height() + yTopMedia;
+
+                if(scrollTop < yBottomMedia && scrollBottom > yTopMedia){
+                    $(this).get(0).play();
+                } else {
+                    $(this).get(0).pause();
+                }
+            }, setInterval(1000));
+        } else {}
+    }
+    function checkPausePlay() {
+        $(document).on('scroll', checkMedia);
+    }        
     function hideShowMenuScroll() {
         $(window).scroll(function() {
             scrolling = true;          
@@ -68,43 +78,22 @@ $(document).ready(function() {
         } else {
             removeUnderlinePageCSS("work");
         }          
-        if (url.indexOf("about") > -1  || url.indexOf("work") > -1 || url === "/") {  
-            $("#intro-bio").css({             
-                position: "static",
-                top: "0"
-            });                      
-            $("#intro-bio").delay(200).animate({
-                opacity: "1.0"},
-                {duration: 700, easing: 'easeOutBounce'
-            });                            
+        if (url.indexOf("about") > -1  || url.indexOf("work") > -1 || url === "/") {                           
         } else {
-            $("#intro-bio").css({
-                opacity: "0",             
-                position: "absolute",
-                top: "-50%"
-            });
             removeUnderlinePageCSS("work");
             removeUnderlinePageCSS("about");     
         }                         
     }
     function projectCarouselFlickity() {
         $("#carousel-container").flickity({
+            asNavFor: "#carousel-container",
             cellAlign: "left",
-            freeScroll: true,
-            wrapAround: true,
-            groupCells: true,
             draggable: true,
-            arrowShape: { 
-                x0: 10,
-                x1: 60, y1: 45,
-                x2: 65, y2: 40,
-                x3: 20
-            }
-            // autoPlay: 3500
+            prevNextButtons: false
         });  
     }    
     function reloadOnPopstate() {
-       $(window).on('popstate', function() {
+       $(window).on("popstate", function() {
           location.reload(true);
        });        
     }         
@@ -116,11 +105,11 @@ $(document).ready(function() {
         });                  
     }  
     function scalePicOnHover() {
-        $('.project-container, .for-carousel').on("mouseenter mousedown touchstart", function(){
+        $(".for-carousel").on("mouseenter mousedown touchstart", function(){
             $(this).find("img.thumbnail").css("transform", "scale(1.05)");
             $(this).find(".project-description").removeClass("hide");  
         })
-        $('.project-container, .for-carousel').on("mouseleave mouseup touchend", function(){
+        $(".for-carousel").on("mouseleave mouseup touchend", function(){
             $(this).find("img.thumbnail").css("transform", "scale(1)");
             $(this).find(".project-description").addClass("hide");      
         })            
@@ -128,12 +117,6 @@ $(document).ready(function() {
     function staticRevealHideClass() { 
         if (url.indexOf("about") > -1 ) {
             revealHideClass("about");            
-        } else
-        if (url.indexOf("modu") > -1 ) {
-            revealHideClass("modu");
-        } else
-        if (url.indexOf('hanban') > -1 ) {
-            revealHideClass("hanban");
         } else
         if (url.indexOf("pathwise") > -1 ) {
             revealHideClass("pathwise");
@@ -171,19 +154,17 @@ $(document).ready(function() {
     //   
     //    
     //
+    checkPausePlay();   
     pageNavCSS();
     staticRevealHideClass();
     scalePicOnHover();
-    changeHanbanCut();
     reloadOnPopstate();
     projectCarouselFlickity();
     updateScrollPos();
     hideShowMenuScroll();
     loadPageContent("about", "About");
     loadPageContent("work", "Work");    
-    loadPageContent("modu", "mod.u Modular Clothing");
     loadPageContent("pathwise", "Pathwise Credit Union");
-    loadPageContent("hanban", "Hanban Fusion Typeface");
     loadPageContent("raw-pet-food", "Raw Pet Food Subscription Service");
 //
 });
