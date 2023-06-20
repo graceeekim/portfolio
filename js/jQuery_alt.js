2d60d3bded201f3e234ac418214ac8037688f906
graceeekim <gracekimj02@msn.com>
2023-06-20 08:03:46 -0400

    Add files via upload

--- js/jQuery_alt.js
+++ js/jQuery_alt.js
@@ -2,10 +2,7 @@ $(document).ready(function() {
     var scrollTop = window.scrollY;
     var scrolling = false;    
     var prevScrollPos = 0;
-    var hidingMenu;
-    var url = window.location.pathname; 
-    var media = $("video").not("[autoplay='autoplay']");
-    var tolerancePixel = 40;    
+    var hidingMenu;   
     //      
     function checkScrollPos() {
         var currentScrollPos = scrollTop;     
@@ -27,53 +24,7 @@ $(document).ready(function() {
                 scrolling = true;    
             }) 
         hidingMenu = setInterval(checkScrollPos, 200);
-    }    
-    function pageNavCSS() {
-        function removeUnderlinePageCSS(pageURL) {
-            $("#menu-" + pageURL).css("border-bottom", "none");
-            $("#menu-" + pageURL).on("mouseenter mousedown touchstart", function(){
-                $("#menu-" + pageURL).css("border-bottom", "none"); 
-            })
-            $("#menu-" + pageURL).on("mouseleave mouseup touchend", function(){
-                $("#menu-" + pageURL).css("border-bottom", "none");     
-            })  
-        }
-        function underlinePageNav(pageURL) {
-            $("#menu-" + pageURL).css("border-bottom", "1px solid black");
-            $("#menu-" + pageURL).on("mouseenter mousedown touchstart", function(){
-                $("#menu-" + pageURL).css("border-bottom", "1px solid #f37053"); 
-            })
-            $("#menu-" + pageURL).on("mouseleave mouseup touchend", function(){
-                $("#menu-" + pageURL).css("border-bottom", "1px solid black");     
-            })   
-        }
-        var url = window.location.pathname;
-        if (url.indexOf("about") > -1 ) {
-            underlinePageNav("about");        
-        } else {
-            removeUnderlinePageCSS("about");  
-        }     
-        if (url === "/" || url.indexOf("work") > -1 || url.indexOf("index") > -1 ) {
-            underlinePageNav("index"); 
-        } else {
-            removeUnderlinePageCSS("index");
-        }          
-        if (url.indexOf("about") > -1  || url.indexOf("work") > -1 || url === "/" || url.indexOf("index") > -1 ) {                           
-        } else {
-            removeUnderlinePageCSS("index");
-            removeUnderlinePageCSS("about");     
-        }                         
-    }   
-    function scalePicOnHover() {
-        $(".for-carousel").on("mouseenter mousedown touchstart", function(){
-            $(this).find("img.thumbnail").css("transform", "scale(1.05)");
-            // $(this).find(".project-description").removeClass("hide");  
-        })
-        $(".for-carousel").on("mouseleave mouseup touchend", function(){
-            $(this).find("img.thumbnail").css("transform", "scale(1)");
-            // $(this).find(".project-description").addClass("hide");      
-        })            
-    }            
+    }              
     function updateScrollPos() {
         $(window).scroll(function() {
             scrollTop = window.scrollY;                        
@@ -89,10 +40,7 @@ $(document).ready(function() {
             }
         });
     }
-
-    checkPausePlay(); 
-    pageNavCSS();
-    scalePicOnHover();
+    hideShowMenuScroll();
     updateScrollPos();
     updatePicOnHover();
 
